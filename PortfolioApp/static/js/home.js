3c6ee48df563354a6ba4d04871a1ef9958600d94
Tushar <43941997+tusharprajapati815@users.noreply.github.com>
2024-07-20 01:11:32 +0530

    Initial commit

---
+++ PortfolioApp/static/js/home.js
@@ -0,0 +1,6 @@
+function togglemenu() {
+  const menu = document.querySelector(".menu-link");
+  const icon = document.querySelector(".humburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
